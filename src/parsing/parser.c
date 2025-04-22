e97ccf2366feb3da658b3216e6f49ee9942e500e
AdryzR <adrien.raphanaud@epitech.eu>
2025-04-22 14:39:25 +0200

    fix(parser_parse_program): allocate and realloc dynamically the three when appending a node to it

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -7,35 +7,61 @@
 
 #include <parser.h>
 
-ast_t *parser_parse_program(parser_t *parser)
+ast_t *create_ast(ast_type_t type)
 {
-    ast_t *node;
+    ast_t *ast = malloc(sizeof(ast_t));
 
-    //todo: switch case selon le type de parser->previous ou parser->current ??
+    ast->type = type;
+    return ast;
+}
 
-    //todo: parser_parse_[...](node)
+ast_t *parser_parse_redirect(parser_t *parser)
+{
+    ast_t *node = create_ast(AT_REDIRECT);
+    redir_node_t redir;
 
+    switch (parser->current.type)
+    {
+    case TT_REDIRECT_OUT:
+        redir.red_type = REDIR_OUT;
+        redir.path = get_token_value(&parser->current);
+        node->data.redirect = redir;
+        break;
+    default:
+        break;
+    }
+}
+
+ast_t *parse_statement(parser_t *parser)
+{
+    switch (parser->current.type) {
+    case TT_REDIRECT_IN:
+    case TT_REDIRECT_OUT:
+    case TT_APPEND:
+    case TT_HEREDOC:
+        return parser_parse_redirect(parser);
+    default:
+        break;
+    }
 }
 
 ast_t *parser_parse_program(parser_t *parser)
 {
     ast_t *tree = create_ast(AT_PROGRAM);
     ast_t *current;
-    size_t i = 0;
 
     while (IS_SEPARATOR(parser->current.type)) {
         parser_skip_separators(parser);
         if (parser->current.type == TT_EOF)
             break;
         current = parse_statement(parser);
-        current->data.program[i] = current;
-        i++;
+        ast_list_append(&tree->data.program, current);
         if (current->type == AT_ERROR)
             break;
     }
     //todo: gestion d'erreur à mettre ici
     if (parser->current.type != TT_EOF) {
-        dprintf(2 , "ERROR IN PARSING\n");
+        fputs("ERROR IN PARSING\n", stderr);
         exit(150);
     }
     return tree;
@@ -48,13 +74,16 @@ parser_t *parser_get(void)
     return &parser;
 }
 
+/**
+ * Dans le main: créer lexer et initialiser pos et start à 0 avant de call ça
+**/
 ast_t *parser_parse(lexer_t *lexer)
 {
     parser_t *parser = parser_get();
     ast_t *ast;
 
-    parser->prev = get_next_token(&lexer);
-    parser->current = parser->prev;
+    parser->prev = (token_t){ TT_EOF, 0 };
+    parser->current = get_next_token(&lexer);
     parser->next = get_next_token(&lexer);
     ast = parser_parse_program(parser);
     return ast;
