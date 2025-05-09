6ff12ece47c6298f2b320c6c9468aed6e6f06b67
AdryzR <adrien.raphanaud@epitech.eu>
2025-05-09 16:30:30 +0200

    feat: add argument globing in interpreter

--- src/interpret_ast/interpret_argument.c
+++ src/interpret_ast/interpret_argument.c
@@ -7,22 +7,32 @@
 
 #include <my_sh.h>
 
+static int glob_error(glob_t *to_free, int ret)
+{
+    globfree(to_free);
+    return 84;
+}
+
 int interpret_argument(const ast_t *ast, shell_t *shell)
 {
-    size_t array_size;
+    glob_t glb = globbing(ast->data.arg);
+    size_t matches = glb.gl_pathc;
+    size_t to_add = matches ? matches : 1;
+    size_t old = len_array(shell->command);
+    size_t total = old + to_add + 1;
+    char **tmp = (shell->command ? reallocarray(shell->command, total,
+        sizeof(*tmp)) : calloc(total, sizeof(*tmp)));
 
-    if (!shell->command) {
-        shell->command = calloc(2, sizeof(char *));
-        if (!shell->command)
-            return 84;
-        shell->command[0] = strdup(ast->data.arg);
-        return 0;
+    if (!tmp)
+        return glob_error(&glb, 84);
+    shell->command = tmp;
+    if (matches) {
+        for (size_t i = 0; i < matches; ++i)
+            shell->command[old + i] = strdup(glb.gl_pathv[i]);
+    } else {
+        shell->command[old] = strdup(ast->data.arg);
     }
-    array_size = len_array(shell->command);
-    shell->command = reallocarray(
-        shell->command, array_size + 2, sizeof(char *)
-    );
-    shell->command[array_size] = strdup(ast->data.arg);
-    shell->command[array_size + 1] = NULL;
+    shell->command[old + to_add] = NULL;
+    globfree(&glb);
     return 0;
 }
