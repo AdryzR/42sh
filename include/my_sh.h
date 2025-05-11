bcc77376a5b1ec541c985f83e352a453ad78112b
AdryzR <adrien.raphanaud@epitech.eu>
2025-05-11 12:58:58 +0200

    fix: clean up my_sh.h

--- include/my_sh.h
+++ include/my_sh.h
@@ -92,48 +92,58 @@ typedef struct shell_s {
     char *input;
 } shell_t;
 
+// Commands handling
+int check_shell_args(shell_t *shell);
+int check_commands(shell_t *shell, char *command, bool print);
+int execute_cmd(shell_t *box);
+
+// Builtins
 typedef int (*builtin_fn_t)(shell_t *shell);
 int builtin_exit(shell_t *shell);
+int is_a_built_in(shell_t *shell, char *command, bool exec);
+int my_exit(shell_t *shell, int exit_status);
+int my_unsetenv(shell_t *shell);
+int my_env(shell_t *shell);
+int my_cd(shell_t *shell);
+int my_setenv(shell_t *shell);
+int my_echo(shell_t *shell);
+int my_repeat(shell_t *shell);
+int my_which(shell_t *shell);
+int my_where(shell_t *shell);
 
+
+// Redirections
 int make_redirect_out(shell_t *shell, char *filename, redir_type_t type);
 int make_redirect_in(shell_t *shell, char *filename);
 int make_redir_heredoc(shell_t *shell, const char *eof);
 
+// Utilities & Program handling
 void main_loop(shell_t *shell, ssize_t bytes_read);
 
 glob_t globbing(char *pattern);
 
 void setup_path_copy(shell_t *shell);
 int wait_for_pid(shell_t *shell, int c_pid);
-int is_a_built_in(shell_t *shell, char *command, bool exec);
-int missing_name_err(char **commands, shell_t *shell);
 char **str_to_warray(char *str, char *delim);
-int left_to_right(shell_t *shell);
 char *strip_str(char *str, char remove);
-int handle_redirections(shell_t *shell);
 int ret_and_set_status(int ret, shell_t *shell);
 char *my_getenv(shell_t *shell, char *tofind);
 void add_env_line(char *env, shell_t *shell);
 void delete_env_node(envi_t *current, shell_t *shell);
 char *my_getcwd(void);
-int my_cd(shell_t *shell);
 void init_struct(shell_t *shell, char **env);
 void free_array(char **array);
 char *my_strcat(char *dest, char const *src);
 char *my_strcat_m(char *dest, char const *src);
 int my_strncmp(char const *s1, char const *s2, int size);
-int my_unsetenv(shell_t *shell);
-int my_env(shell_t *shell);
 int new_env_line(envi_t *buff, shell_t *shell);
-int check_shell_args(shell_t *shell);
-int my_setenv(shell_t *shell);
 void free_all(shell_t *shell);
-int check_commands(shell_t *shell, char *command, bool print);
-int my_exit(shell_t *shell, int exit_status);
-int execute_cmd(shell_t *box);
 int my_putstr_ch(int fd, char const *str);
-int my_echo(shell_t *shell);
+void print_prompt(shell_t *shell);
+char *word_array_to_str(char **tab, char *delim, int start);
+lexer_t update_lexer(lexer_t lexer, char *line);
 
+// Read usage
 char *read_line(shell_t *shell);
 char *arrows_key(shell_t *shell, history_t *hist, char c, char *line);
 
@@ -144,14 +154,8 @@ void set_index(shell_t *shell, history_t *hist);
 int print_history(void);
 int exec_print_history(shell_t *shell);
 
-void print_prompt(shell_t *shell);
-int my_which(shell_t *shell);
-int my_where(shell_t *shell);
-lexer_t update_lexer(lexer_t lexer, char *line);
-int my_repeat(shell_t *shell);
-char *word_array_to_str(char **tab, char *delim, int start);
 
-// alias
+// Handle aliases
 int my_alias(shell_t *shell);
 char *replace_aliases(shell_t *shell, char *input, alias_t *aliases);
 alias_t *find_alias(alias_t *head, const char *name);
