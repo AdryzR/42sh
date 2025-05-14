d1f48d207b67d562b387047f05efbe625f0feea3
AdryzR <adrien.raphanaud@epitech.eu>
2025-05-14 09:34:37 +0200

    feat: add status to the prompt

--- src/utilities/print_prompt.c
+++ src/utilities/print_prompt.c
@@ -12,6 +12,10 @@ void print_prompt(shell_t *shell)
     char path[SH_PATH_MAX];
     char *user = my_getenv(shell, "USER");
 
+    if (shell->shell_status == 0)
+        dprintf(1, "\033[\033[1;32m[%d] ", shell->shell_status);
+    else
+        dprintf(1, "\033[38;5;214m[%d] ", shell->shell_status);
     if (user != NULL)
         printf("\033[0;32m@%s", user);
     if (!user)
